809e598f22a142e7af96e40d0803500b3c87b7bb
Satyam Gupta <120209121+satya-supercluster@users.noreply.github.com>
2024-05-26 14:30:47 +0530

    Time: 1297 ms (30.77%), Space: 194.5 MB (7.69%) - LeetHub

---
+++ 3444-find-the-number-of-good-pairs-ii/3444-find-the-number-of-good-pairs-ii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    long long numberOfPairs(vector<int>& nums1, vector<int>& nums2, int k) {
+        unordered_map<int,int> hashTable;
+        
+        for(auto number : nums1){
+            
+            for(int i = 1; i * i <= number; i++){
+                if(i * i == number){
+                    hashTable[i]++;
+                }else if(number % i == 0){
+                    hashTable[i]++;
+                    hashTable[number / i]++;
+                }
+            }
+            
+        }
+        
+        long long int answer = 0;
+        
+        for(auto number : nums2){
+            number *= k;
+            if(hashTable[number] > 0){
+                answer += hashTable[number];
+            }
+        }
+        
+        return answer;
+    }
+};
