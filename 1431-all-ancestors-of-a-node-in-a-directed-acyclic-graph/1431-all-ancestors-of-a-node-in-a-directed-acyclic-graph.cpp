94b3d0bf19184783d344182d5ef4fa341922bc85
Satyam Gupta <120209121+satya-supercluster@users.noreply.github.com>
2024-07-04 00:03:09 +0530

    Time: 494 ms (5.13%), Space: 240.9 MB (5%) - LeetHub

--- 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
+++ 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
@@ -1,35 +1,37 @@
 class Solution {
 public:
-    set<int> dfs(int i, vector<vector<int>>& parents, vector<vector<int>>& adj){
-        set<int> s;
-        for(auto it : adj[i]){
-            if(parents[it].size() == 0){
-                set<int> t =  dfs(it,parents,adj);
-                s.insert(t.begin(),t.end());
-            }
-            else{
-                s.insert(parents[it].begin(),parents[it].end());
-                s.insert(it);
-            }
-        }
-        parents[i] = {s.begin(),s.end()};
-        s.insert(i);
-        return s;
+    vector<int> merge(vector<int>&a,vector<int>&b){
+        set<int>s;
+        for(auto&i:a)s.insert(i);
+        for(auto&i:b)s.insert(i);
+        vector<int> ans(begin(s),end(s));
+        return ans;
     }
     vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) {
-        vector<vector<int>> parents(n);
-        // vector<set> parents(n);
         vector<vector<int>> adj(n);
-        vector<int> outDeg(n,0);
-        for(auto it : edges){
-            adj[it[1]].push_back(it[0]);
-            outDeg[it[0]]++;
+        vector<int> indeg(n,0);
+        for(auto&it:edges){
+            adj[it[0]].push_back(it[1]);
+            indeg[it[1]]++;
         }
-        for(int i = 0; i < n; i++){
-            if(outDeg[i] == 0){
-                set<int> temp = dfs(i,parents,adj);
+        queue<int>q;
+        for(int i=0;i<n;i++){
+            if(indeg[i]==0)q.push(i);
+        }
+        vector<vector<int>>ans(n);
+        while(!q.empty()){
+            auto e=q.front();
+            q.pop();
+            for(auto&child:adj[e]){
+                vector<int> temp=ans[e];
+                temp.push_back(e);
+                ans[child]=merge(temp,ans[child]);
+                indeg[child]--;
+                if(indeg[child]==0){
+                    q.push(child);
+                }
             }
         }
-        return parents;
+        return ans;
     }
 };
