0eb0eba3e146286abb622b06c93770fa2a296941
Satyam Gupta <120209121+satya-supercluster@users.noreply.github.com>
2024-07-04 18:03:03 +0530

    Time: 72 ms (21.3%), Space: 13.3 MB (81.9%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,40 +1,28 @@
 class Solution {
 public:
-    bool solve(int i, int j, string& s, string& p, vector<vector<int>>& dp){
-        if( i < 0 && j < 0){
-            return true;
+    bool isMatch(string &s, string &p) {
+        int n=p.length(),m=s.length();
+        vector<vector<bool>>dp(n+1,vector<bool>(m+1,false));
+        dp[0][0]=true;
+        for(int i=1;i<=n;i++){
+            if(p[i-1]=='*'){
+                dp[i][0]=true;
+            }
+            else break;
         }
-        if( j < 0){
-            return false;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(p[i-1]==s[j-1])dp[i][j]=dp[i-1][j-1];
+                else if(p[i-1]=='?')dp[i][j]=dp[i-1][j-1];
+                else if(p[i-1]=='*')dp[i][j]=dp[i-1][j-1]|dp[i][j-1]|dp[i-1][j];
+            }
         }
-        if( i < 0){
-            while(j >= 0 &&( p[j] =='*') )
-                j--;
-            return j < 0;
-        }
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
-        if(p[j] == '?'){
-            // cout << 01 << " " <<  solve(i-1,j-1,s,p) << endl;
-            // cout << 02 << " " <<  solve(i,j-1,s,p) << endl;
-            return dp[i][j] = (solve(i-1,j-1,s,p,dp));
-        }
-        if(p[j] == '*'){
-            // cout << i << j << " " <<  solve(i-1,j,s,p) << endl;
-            // cout << i << j << " " <<  solve(i,j-1,s,p) << endl;
-            return  dp[i][j] =(solve(i-1,j,s,p,dp)|solve(i,j-1,s,p,dp));
-        }
-
-        if(s[i] == p[j]){
-             return dp[i][j] = solve(i-1,j-1,s,p,dp);
-        }
-        return dp[i][j] = false;
-    }
-
-    bool isMatch(string s, string p) {
-        int n1 = s.size(), n2 = p.size();
-            vector<vector<int>> dp(n1,vector<int>(n2,-1));
-        return solve(n1-1,n2-1,s,p,dp);
+        // for(int i{};i<=n;i++){
+        //     for(int j{};j<=m;j++){
+        //         cout<<dp[i][j]<<" ";
+        //     }
+        //     cout<<endl;
+        // }
+        return dp[n][m];
     }
 };
