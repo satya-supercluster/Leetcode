2b4a9b8ebbb9944d4724e66f64f5bc54bec23468
Satyam Gupta <120209121+satya-supercluster@users.noreply.github.com>
2024-12-26 16:13:22 +0530

    Time: 2 ms (90.56%), Space: 12.2 MB (63.51%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
-    int findTargetSumWays(vector<int>& v, int target) {
+    int findTargetSumWays(vector<int>& nums, int target) {
         int totalSum = 0;
-        for (int num : v) totalSum += num;
+        for (int num : nums) totalSum += num;
+        
         if (abs(target) > totalSum) return 0;
         if ((totalSum + target) % 2 != 0) return 0;
-        target = (totalSum + target) / 2;
-        int n=v.size();
-        vector<vector<int>>dp(n+1,vector<int>(target+1,0));
-        dp[0][0]=1;
-        for(int i{1};i<=n;i++){
-            for(int j{0};j<=target;j++){
-                if(j>=v[i-1]){
-                    int take=dp[i-1][j-v[i-1]];
-                    int not_take=dp[i-1][j];
-                    dp[i][j]=take+not_take;
-                }
-                else{
-                    dp[i][j]=dp[i-1][j];
-                }
+        
+        int newTarget = (totalSum + target) / 2;
+        
+        vector<int> dp(newTarget + 1, 0);
+        dp[0] = 1;
+        
+        for (int num : nums) {
+            for (int j = newTarget; j >= num; j--) {
+                dp[j] += dp[j - num];
             }
         }
-        return dp[n][target];
+        
+        return dp[newTarget];
     }
 };
