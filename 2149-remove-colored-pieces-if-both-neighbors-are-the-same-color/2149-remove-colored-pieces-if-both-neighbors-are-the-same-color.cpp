88cf1085b17528bd79984a003fd6638ea5e9785e
Satyam Gupta <120209121+satya-supercluster@users.noreply.github.com>
2025-01-09 15:33:36 +0530

    Time: 15 ms (9.41%), Space: 15.6 MB (54.95%) - LeetHub

---
+++ 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/2149-remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool winnerOfGame(string v) {
+        int ca{},cb{};
+        int curr{0};
+        for(int i{1};i<v.size()-1;i++){
+            if(v[i]=='A' and v[i+1]=='A' and v[i-1]=='A'){
+                ca++;
+            }
+        }
+        for(int i{1};i<v.size()-1;i++){
+            if(v[i]=='B' and v[i+1]=='B' and v[i-1]=='B'){
+                cb++;
+            }
+        }
+        if(ca<=cb) return false;
+        return true;
+    }
+};
