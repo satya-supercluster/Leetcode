35773d716eb18d0729b471f986d6a09c8ec7c2b6
Satyam Gupta <120209121+satya-supercluster@users.noreply.github.com>
2025-01-09 15:34:38 +0530

    Time: 9 ms (24.01%), Space: 15.7 MB (35.89%) - LeetHub

--- 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/2149-remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
+++ 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/2149-remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
@@ -7,8 +7,6 @@ public:
             if(v[i]=='A' and v[i+1]=='A' and v[i-1]=='A'){
                 ca++;
             }
-        }
-        for(int i{1};i<v.size()-1;i++){
             if(v[i]=='B' and v[i+1]=='B' and v[i-1]=='B'){
                 cb++;
             }
