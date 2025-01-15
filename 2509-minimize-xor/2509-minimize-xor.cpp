45a7bfa8a146ac7c515da91db897ebb58f65b9fd
Satyam Gupta <120209121+satya-supercluster@users.noreply.github.com>
2025-01-15 16:57:08 +0530

    Time: 0 ms (100%), Space: 7.9 MB (64.02%) - LeetHub

---
+++ 2509-minimize-xor/2509-minimize-xor.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int minimizeXor(int num1, int num2) {
+        int c=__builtin_popcount(num2);
+        int c1=__builtin_popcount(num1);
+        if(c==c1) return num1;
+        if(c<c1){
+            int ans{};
+            for(int i{31};i>=0;i--){
+                if(num1&(1ll<<i)){
+                    if(c>0){
+                        ans|=(1ll<<i);
+                        c--;
+                    }
+                }
+            }
+            return ans;
+        }
+        else{
+            int ans{};
+            c-=c1;
+            for(int i{};i<32;i++){
+                if((num1&(1ll<<i))==0){
+                    if(c>0){
+                        ans|=(1ll<<i);
+                        c--;
+                    }
+                }
+                else ans|=(1ll<<i);
+            }
+            return ans;
+        }
+        return 0;
+    }
+};
