48827a44acb1f9cf30344c2074888ffbf9a0d0a1
Satyam Gupta <120209121+satya-supercluster@users.noreply.github.com>
2025-02-11 15:45:49 +0530

    Time: 0 ms (100%), Space: 52.3 MB (30.71%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,24 +1,13 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int n=nums.size();
-        if(n==1) return true;
-        if(nums[0]==0) return false;
-        vector<int> diff(n+1,0);
-        diff[min(1,n)]+=1;
-        diff[min(nums[0]+1,n)]-=1;
-        for(int i{1};i<n;i++){
-            if(i>0) diff[i]+=diff[i-1];
-            if(diff[i]>0){
-                if(nums[i]>0){
-                    diff[min(i+1,n)]+=1;
-                    diff[min(i+nums[i]+1,n)]-=1;
-                }
+        int maxInd = 0;
+        for(int i=0; i<nums.size(); i++){
+            if(i>maxInd){
+                return false;
             }
+            maxInd = max(maxInd,i+nums[i]);
         }
-        for(auto&i:diff) cout<<i<<" ";
-        cout<<endl;
-        if(diff[n-1]>0) return true;
-        return false;
+        return true;
     }
 };
