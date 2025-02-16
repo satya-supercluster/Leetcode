6058ce29019656e4a91a8bae59e4db4b21a30d39
Satyam Gupta <120209121+satya-supercluster@users.noreply.github.com>
2025-02-16 21:46:05 +0530

    Time: 0 ms (100%), Space: 13.9 MB (59.72%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.cpp
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -5,15 +5,11 @@ public:
         for(auto&i:nums)x^=i;
         int ind{-1};
         ind=x&(-(unsigned)x);
-        vector<int> f,s;
+        int x1{},x2{};
         for(auto&i:nums){
-            if(i&ind)f.push_back(i);
-            else s.push_back(i);
+            if(i&ind) x1^=i;
+            else x2^=i;
         }
-        int x1=x;
-        for(auto&i:f)x1^=i;
-        int x2=x;
-        for(auto&i:s)x2^=i;
         return {x1,x2};
     }
 };
