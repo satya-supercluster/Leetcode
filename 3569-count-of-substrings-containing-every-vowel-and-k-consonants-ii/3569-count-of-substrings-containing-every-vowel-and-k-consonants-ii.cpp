1a1de8d67ce3b9aec038fbf974521bf4b6aeda6c
Satyam Gupta <120209121+satya-supercluster@users.noreply.github.com>
2025-03-10 16:26:44 +0530

    Time: 243 ms (36.16%), Space: 54.1 MB (42.47%) - LeetHub

--- 3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii/3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii.cpp
+++ 3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii/3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii.cpp
@@ -1,38 +1,28 @@
 class Solution {
 public:
-    long long Solve(string& word, int k) {
-        int n = word.length();
-        int i = 0, j = 0;
-        long long ans = 0;
-        map<char, int> mp;
-        int cnt = 0;
-
-        while (j < n) {
-            if (word[j] == 'a' || word[j] == 'e' || word[j] == 'i' || word[j] == 'o' || word[j] == 'u') {
-                mp[word[j]]++;
-            } else {
-                cnt++;
-            }
-
-            while (mp.size() == 5 && cnt >= k) {
-                ans += word.size() - j;
-                
-                if (mp.find(word[i]) != mp.end()) {
-                    mp[word[i]]--;
-                    if (mp[word[i]] == 0) {
-                        mp.erase(word[i]);
-                    }
-                } else {
-                    cnt--;
+    bool isVowel(char c){
+        if(c=='a' or c=='e' or c=='i' or c=='o' or c=='u') return true;
+        return false;
+    }
+    long long solve(string word, int k) {
+        map<char,int> vow;
+        long long cons{},i{},ans{};
+        for(int j{};j<word.length();j++){
+            if(isVowel(word[j])) vow[word[j]]++;
+            else cons++;
+            while(cons>=k and vow.size()==5 and i<j){
+                ans+=word.length()-j;
+                if(isVowel(word[i])) {
+                    vow[word[i]]--;
+                    if(vow[word[i]]==0) vow.erase(word[i]);
                 }
+                else cons--;
                 i++;
             }
-            j++;
         }
         return ans;
     }
-
     long long countOfSubstrings(string word, int k) {
-        return Solve(word, k) - Solve(word, k + 1);
+        return solve(word,k)-solve(word,k+1);
     }
 };
